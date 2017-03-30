8551684dad657a4059549216f5a063fcd7543393
Mirna Plakalovic <mirna.plakalovic@gmail.com>
2017-03-30 01:19:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  var newArray = [];
+  for (var i = 0; i < dwarves.length; i++) {
+    newArray.push(`${i+1}. ${dwarves[i]}`);
+  }
+  return `${newArray.join(' ')} `;
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i] = `${planeteerCalls[i]}.toUppercase()!`;
+  }
+  return planeteerCalls;
 }
+
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+      if (words[i].length > 4) {
+        return true;
+      }
+  }
+  return false;
 }
 
-function findTheCheese (foods) {
+function findTheCheese(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 'cheddar' || array[i] === 'gouda' || array[i] === 'camembert') {
+      return array[i] 
+    } 
+}
+  return "no cheese!";
 }
